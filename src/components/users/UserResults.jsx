dde8391d1c0454baa51d71936c070e3b6e44b2c9
Omole Daniel <danielomole18@gmail.com>
2023-04-19 23:02:22 +0100

    editted the userResults component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,23 @@
+import {useEffect} from 'react'
+
 function UserResults() {
+  useEffect(() => {
+    fetchUsers()
+  },[])
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${import.meta.env.VITE_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token $ {
+          import.meta.env.VITE_APP_GITHUB_TOKEN
+        }`
+      }
+    })
+
+    const data = await response.json()
+
+    console.log(data)
+  }
   return <div>User Results</div>
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import UserResults from "../components/users/UserResults"
 function Home() {
   return <div>
     <UserResults/>
+    {/* <h1 className="text-6xl">Welcome</h1>
+    {import.meta.env.VITE_APP_GITHUB_TOKEN} */}
   </div>
 }
 
